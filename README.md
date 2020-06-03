a3da0a142879b65904376cdd718929e104703f96
liuteng <1161653410@qq.com>
2020-06-03 12:19:54 +0800

    sweger

--- README.md
+++ README.md
@@ -3,23 +3,23 @@
 # 相关使用说明
 - **nacos.yml** : *nacos配置文件*
 
-    url : http://106.13.181.6:8848/nacos
+    - url : http://106.13.181.6:8848/nacos
     
-    账号：nacos   密码：nacos
+    - 账号：nacos   密码：nacos
     
 - **user_index.json** : *ES索引文件*
   
   - 创建索引
     
-    url : 106.13.181.6:9200/user
+    - url : 106.13.181.6:9200/user
     
-    method : PUT
+    - method : PUT
     
   - 添加索引字段
    
-     method : POST
+     - method : POST
      
-     url : http://106.13.181.6:9200/user/user/_mapping
+     - url : http://106.13.181.6:9200/user/user/_mapping
   
    - ElasticHD : http://106.13.181.6:9800/
   
@@ -27,6 +27,10 @@
 
 - **XXL-Job** : *分布式统一调度平台*
   
-  url : http://106.13.181.6:9090/xxl-job-admin/
+  - url : http://106.13.181.6:9090/xxl-job-admin/
   
-  账号 ：admin 密码：123456
+  - 账号 ：admin 密码：123456
+  
+- Sweger
+  
+  - url : http://localhost:8080/swagger-ui.html
--- src/main/java/com/xiaoxi/study/controller/UserController.java
+++ src/main/java/com/xiaoxi/study/controller/UserController.java
@@ -1,15 +1,14 @@
 package com.xiaoxi.study.controller;
 
 import com.xiaoxi.study.common.Result;
-import com.xiaoxi.study.dto.request.CreateUserRequestDTO;
-import com.xiaoxi.study.dto.request.GetUserByIdRequestDTO;
-import com.xiaoxi.study.dto.response.CreateUserResponseDTO;
-import com.xiaoxi.study.dto.response.GetUserByIdResponseDTO;
+import com.xiaoxi.study.dto.request.*;
+import com.xiaoxi.study.dto.response.*;
+import com.xiaoxi.study.service.EsService;
 import com.xiaoxi.study.service.UserService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,21 +18,50 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping(value = "/demo")
+@Api(value = "用户信息接口", tags = "UserController")
 public class UserController {
 
     private UserService userService;
 
-    public UserController(UserService userService) {
+    private EsService esService;
+
+    public UserController(UserService userService, EsService esService) {
         this.userService = userService;
+        this.esService = esService;
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public Result<CreateUserResponseDTO> create(@Valid @RequestBody CreateUserRequestDTO request){
+    @ApiOperation(value = "创建用户", notes = "创建用户")
+    public Result<CreateUserResponseDTO> create(@ApiParam(required = true, name = "request", value = "创建用户请求实体")
+                                                @Valid @RequestBody CreateUserRequestDTO request){
         return new Result<>(userService.save(request));
     }
 
     @RequestMapping(value = "/getUserById", method = RequestMethod.POST)
-    public Result<GetUserByIdResponseDTO> getUserById(@Valid @RequestBody GetUserByIdRequestDTO request){
+    @ApiOperation(value = "通过ID查询用户", notes = "通过ID查询用户")
+    public Result<GetUserByIdResponseDTO> getUserById(@ApiParam(required = true, name = "request", value = "查询用户信息请求实体")
+                                                      @Valid @RequestBody GetUserByIdRequestDTO request){
         return new Result<>(userService.getUserById(request));
+    }
+
+    @PostMapping(value = "getUserByPhone")
+    @ApiOperation(value = "通过手机号查询用户", notes = "通过手机号查询用户")
+    public Result<GetUserByPhoneResponseDTO> getUserByPhone(@ApiParam(required = true, name = "request", value = "查询用户信息请求实体")
+                                                            @Valid @RequestBody GetUserByPhoneRequestDTO request) {
+        return new Result<>(esService.getUserByPhone(request));
+    }
+
+    @PostMapping(value = "getUserByName")
+    @ApiOperation(value = "通过姓名查询用户", notes = "通过姓名查询用户")
+    public Result<GetUserByNameResponseDTO> getUserByName(@ApiParam(required = true, name = "request", value = "查询用户信息请求实体")
+                                                          @Valid @RequestBody GetUserByNameRequestDTO request) {
+        return new Result<>(esService.getUserByName(request));
+    }
+
+    @PostMapping(value = "search")
+    @ApiOperation(value = "条件分页查询", notes = "条件分页查询")
+    public Result<SearchUserResponseDTO> search(@ApiParam(required = true, name = "request", value = "查询用户信息请求实体")
+                                                @Valid @RequestBody SearchUserRequestDTO request) {
+        return new Result<>(esService.search(request));
     }
 }
