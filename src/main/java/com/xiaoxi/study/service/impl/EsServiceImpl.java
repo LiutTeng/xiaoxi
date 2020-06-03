b700aad5492d0fb013adf7a3e2bbac564aa27893
liuteng <1161653410@qq.com>
2020-06-03 08:56:48 +0800

    修改索引结构

--- src/main/java/com/xiaoxi/study/service/impl/EsServiceImpl.java
+++ src/main/java/com/xiaoxi/study/service/impl/EsServiceImpl.java
@@ -17,6 +17,8 @@ import java.util.Optional;
 @Service
 public class EsServiceImpl implements EsService {
 
+    private static final String FORMAT = "*%s*";
+
     private EsMapper esMapper;
 
     public EsServiceImpl(EsMapper esMapper) {
@@ -26,6 +28,7 @@ public class EsServiceImpl implements EsService {
     @Override
     public List<UserEsEntity> getUserByPhone(String phone) {
         List<UserEsEntity> userList = new ArrayList<>();
+        // 精确匹配
         QueryBuilder builder = QueryBuilders.matchQuery("phone", phone);
         Optional.ofNullable(esMapper.search(builder)).ifPresent(res-> res.forEach(userList::add));
         return userList;
@@ -34,7 +37,8 @@ public class EsServiceImpl implements EsService {
     @Override
     public List<UserEsEntity> getUserByName(String name) {
         List<UserEsEntity> userList = new ArrayList<>();
-        QueryBuilder builder = QueryBuilders.matchQuery("name", name);
+        // 模糊查询
+        QueryBuilder builder = QueryBuilders.wildcardQuery("name", String.format(FORMAT, name));
         Optional.ofNullable(esMapper.search(builder)).ifPresent(res-> res.forEach(userList::add));
         return userList;
     }
